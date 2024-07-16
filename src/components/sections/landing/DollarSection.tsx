f04bdc0955784da9ced0cfab3eb9834e5b222207
JustBecerra <justj.becerra@gmail.com>
2024-07-16 14:10:41 -0300

    add loader

--- src/components/sections/landing/DollarSection.tsx
+++ src/components/sections/landing/DollarSection.tsx
@@ -57,10 +57,34 @@ const DollarPrices = () => {
     <section className="mx-auto mt-6 w-[100%]">
       <div
         ref={scrollRef}
-        className="scrollbar-hide flex w-[100%] gap-4 overflow-y-hidden overflow-x-scroll rounded-xl border-2 border-blue-50 px-4 py-6 dark:border-yellow-500 sm:px-6 md:items-center md:gap-8 lg:px-8 2xl:max-w-full"
+        className={`flex w-[100%] ${loading && "justify-center"} gap-4 overflow-y-hidden overflow-x-scroll rounded-xl border-2 border-blue-50 px-4 py-6 scrollbar-hide dark:border-yellow-500 sm:px-6 md:items-center md:gap-8 lg:px-8 2xl:max-w-full`}
       >
         {loading ? (
-          <h1>cargando precios...</h1>
+          <div className="flex w-fit flex-col items-center justify-center rounded-2xl bg-blue-50 p-4 dark:bg-yellow-500">
+            <h1 className="text-center text-black dark:text-white">
+              Cargando Valores...
+            </h1>
+            <svg
+              className="mr-3 h-5 w-5 animate-spin text-black dark:text-white"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
+              ></path>
+            </svg>
+          </div>
         ) : (
           dollarPrices.map((dolar, index) => (
             <div
