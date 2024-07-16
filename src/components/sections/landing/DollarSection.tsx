251ac1212e04833cc7b7dcf7cb61bcfbbb976cb7
JustBecerra <justj.becerra@gmail.com>
2024-07-16 11:07:07 -0300

    add colors for light and dark themes

--- src/components/sections/landing/DollarSection.tsx
+++ src/components/sections/landing/DollarSection.tsx
@@ -57,38 +57,50 @@ const DollarPrices = () => {
     <section className="mx-auto mt-6 w-[100%]">
       <div
         ref={scrollRef}
-        className="scrollbar-hide flex w-[100%] gap-4 overflow-y-hidden overflow-x-scroll bg-white px-4 py-6 sm:px-6 md:items-center md:gap-8 lg:px-8 2xl:max-w-full"
+        className="scrollbar-hide flex w-[100%] gap-4 overflow-y-hidden overflow-x-scroll rounded-xl border-2 border-blue-50 px-4 py-6 dark:border-yellow-500 sm:px-6 md:items-center md:gap-8 lg:px-8 2xl:max-w-full"
       >
         {loading ? (
           <h1>cargando precios...</h1>
         ) : (
           dollarPrices.map((dolar, index) => (
             <div
-              className="flex w-[25%] flex-shrink-0 items-center justify-center gap-4 border-2 border-yellow-500 p-2"
+              className="flex w-[25%] flex-shrink-0 items-center justify-center gap-4 rounded-xl border-2 border-blue-50 p-2 dark:border-yellow-500"
               key={index}
             >
               <div className="flex flex-col">
-                <h1 className="text-center">{dolar.moneda}</h1>
-                <h1 className="text-nowrap text-center">
+                <h1 className="text-center text-black dark:text-white">
+                  {dolar.moneda}
+                </h1>
+                <h1 className="text-nowrap text-center text-black dark:text-white">
                   {dolar.nombre === "Contado con liquidación"
                     ? "CCL"
                     : dolar.nombre}
                 </h1>
               </div>
               <div className="flex flex-col">
-                <h1 className="text-center">Compra: </h1>
-                <h1 className="text-center">{dolar.compra}</h1>
-              </div>
-              <div className="flex flex-col">
-                <h1 className="text-center">Venta: </h1>
-                <h1 className="text-center">{dolar.venta}</h1>
-              </div>
-              <div className="flex w-[fit] flex-col">
-                <h1 className="text-nowrap text-center">Fecha Valor:</h1>
-                <h1 className="text-center">
-                  {dolar.fechaActualizacion.substring(0, 10)}
+                <h1 className="text-center text-black dark:text-white">
+                  Compra:{" "}
+                </h1>
+                <h1 className="text-center text-black dark:text-white">
+                  {dolar.compra}
                 </h1>
               </div>
+              <div className="flex flex-col">
+                <h1 className="text-center text-black dark:text-white">
+                  Venta:{" "}
+                </h1>
+                <h1 className="text-center text-black dark:text-white">
+                  {dolar.venta}
+                </h1>
+              </div>
+              {/* <div className="flex w-[fit] flex-col">
+                <h1 className="text-nowrap text-center text-white">
+                  Fecha Valor:
+                </h1>
+                <h1 className="text-center text-white">
+                  {dolar.fechaActualizacion.substring(0, 10)}
+                </h1>
+              </div> */}
             </div>
           ))
         )}
