7c1361103786d6885ac99d7e982625318bd87f24
JustBecerra <justj.becerra@gmail.com>
2024-08-25 10:15:44 -0300

    add emailJs and init

--- src/pages/api/sendEmail.json.ts
+++ src/pages/api/sendEmail.json.ts
@@ -14,7 +14,6 @@ export const POST: APIRoute = async ({ params, request }) => {
     text,
     reply_to,
   });
-  console.log({send})
   if (send.data) {
     return new Response(
       JSON.stringify({
