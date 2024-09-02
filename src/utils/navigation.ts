bc5d1ec3bf48376480256fb827e983eb3d8d5ad0
JustBecerra <justj.becerra@gmail.com>
2024-09-02 15:32:24 -0300

    add screen

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -4,16 +4,23 @@ const navBarLinks = [
   { name: "Ayuda", url: "/ayuda" },
   { name: "Calculadora", url: "/calculadora" },
   { name: "Contacto", url: "/contacto" },
+  { name: "Incumplimiento", url: "/incumplimiento" },
 ];
 
 const footerLinks = [
   {
     section: "Redes Sociales",
     links: [
-      { name: "Facebook", url: "https://www.facebook.com/profile.php?id=61561059611052&mibextid=LQQJ4d" },
+      {
+        name: "Facebook",
+        url: "https://www.facebook.com/profile.php?id=61561059611052&mibextid=LQQJ4d",
+      },
       { name: "Twitter", url: "https://twitter.com/" },
       { name: "Instagram", url: "https://www.instagram.com/sinergiavalores/" },
-      { name: "Tiktok", url: "https://www.tiktok.com/@sinergia.valores?_t=8o3X7xTxBri&_r=1" },
+      {
+        name: "Tiktok",
+        url: "https://www.tiktok.com/@sinergia.valores?_t=8o3X7xTxBri&_r=1",
+      },
     ],
   },
   {
